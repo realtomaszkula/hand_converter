af89418f9509f0d7140054695ae905f5fdf6881d
Tomasz Kula <tomaszkula07@gmail.com>
2016-10-07 21:07:58 +0200

    Add shared and file-reader module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FileReaderModule } from './file-reader/file-reader.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    FileReaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
