aac65ae23fc88deccde1280a74160b0d23f1ddf7
Tomasz Kula <tomaszkula07@gmail.com>
2016-10-08 18:06:09 +0200

    Finish validate service and comp presentation

--- src/app/file-reader/file-reader.component.ts
+++ src/app/file-reader/file-reader.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
 import { Observable } from 'rxjs/Observable';
 
-import { FileValidatorService } from './file-validator.service';
+import { FileValidatorService, ValidateResults} from './file-validator.service';
 
 @Component({
     selector: 'file-reader',
@@ -10,6 +10,14 @@ import { FileValidatorService } from './file-validator.service';
     <input id="files" type="file" multiple 
          #input (change)="onChange(input.files)"/>
 
+         <div *ngIf="validateFinished | async">
+            Uploaded:  
+                <ul> 
+                    <li> Total: {{ (validateResults | async)?.total  }} </li>
+                    <li> Valid: {{ (validateResults | async)?.valid  }} </li>
+                    <li> Invalid: {{ (validateResults | async)?.invalid  }} </li>
+                </ul> 
+        </div>
          <div *ngFor="let msg of validateLog | async">
             {{msg}}
          </div>
@@ -20,10 +28,14 @@ import { FileValidatorService } from './file-validator.service';
 })
 export class FileReaderComponent implements OnInit {
 
-    validateLog: Observable<string[]>
+    validateLog: Observable<string[]>;
+    validateResults: Observable<ValidateResults>;
+    validateFinished: Observable<boolean>;
 
     constructor(private fileValidator: FileValidatorService) {
         this.validateLog = this.fileValidator.validateLog$;
+        this.validateResults = this.fileValidator.validateResults$;
+        this.validateFinished = this.fileValidator.validateFinished$;
     }
 
     ngOnInit() {
--- src/app/file-reader/file-validator.service.ts
+++ src/app/file-reader/file-validator.service.ts
@@ -1,10 +1,12 @@
 import { Injectable, OnInit } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 
-interface ValidateResults {
-   valid: string,
-   invalid: string
+export interface ValidateResults {
+   valid: number,
+   invalid: number,
+   total: number
 }
 
 @Injectable()
@@ -13,8 +15,11 @@ export class FileValidatorService {
     private validateLogSource = new Subject<string[]>();
     validateLog$ = this.validateLogSource.asObservable();
 
-    private validateResultsSource = new Subject<ValidateResults>();
-    validateResults$ = this.validateLogSource.asObservable();
+    private validateFinishedSource = new BehaviorSubject<boolean>(false);
+    validateFinished$ = this.validateFinishedSource.asObservable();
+
+    private validateResultsSource = new BehaviorSubject<ValidateResults>({} as ValidateResults);
+    validateResults$ = this.validateResultsSource.asObservable();
 
     private files: File[];
     private filteredFiles: File[];
@@ -25,16 +30,35 @@ export class FileValidatorService {
     }
 
     private validateNames(): void {
+        this.validateFinishedSource.next(false);
+
         let log: string[] = [];
+        let files = this.files;
+
+        let result: ValidateResults = {
+            total: this.files.length,
+            valid: 0,
+            invalid: 0
+        }
+
         this.filteredFiles = 
             this.files.filter((file: File) => {
                 let isValid = this.validateName(file);
-                if (!isValid) log.push(`${file.name} name is incorrect, accepting only txt files!`);
+
+                if (isValid) {
+                    result.valid++;
+                } else {
+                    result.invalid++;
+                    log.push(`${file.name} name is incorrect, accepting only txt files!`);
+                }
+
                 return isValid;
             })
-
+            console.log(result);
         this.validateLogSource.next(log);
-        console.log(log)
+        this.validateResultsSource.next(result)
+        this.validateFinishedSource.next(true);
+
     }
 
     private validateName(file: File) {
