a50c75cd23b39b24dfb6e59a81ee35a535840ab2
Tomasz Kula <tomaszkula07@gmail.com>
2016-10-09 05:53:45 +0200

    Add seats and blinds replacer functions

--- src/app/file-reader/hand-converter.service.ts
+++ src/app/file-reader/hand-converter.service.ts
@@ -6,6 +6,9 @@ type PipeLineFunction = (string) => string;
 
 @Injectable()
 export class HandConverterService {
+
+    private stakeModifier: number;
+
     /* 
         Capture groups: 
         1st: entire stakes string ex: ($50/$100 USD) or ($50/$100) 
@@ -14,6 +17,24 @@ export class HandConverterService {
         3rd: big blind ex: 100
     */
     private stakesRegExp = /\(\$(\d+)\/\$(\d+)(?:\sUSD\)|\))/;
+
+    /*
+        Capture groups:
+        1st: entire string ex: ($783.50 in chips) 
+        2nd: naked stack ex: 783.50
+    */
+    private stacksRegExp = /\(\$(\d+\.*\d+)\sin chips\)/;
+
+    /*
+        Capture groups: 
+        1st: entire string ex: posts small blind $1
+        2nd: 1
+        3rd: undefined
+        4th: undefined
+
+        only one capture group of 2..4 will return truthy value
+    */
+    private blindsRegExp = /posts small blind \$(\d+)|posts big blind \$(\d+)|posts big & small blind \$(\d+)/;
 
     private hands: string[];
     private _convertedHands: string[];
@@ -55,19 +76,73 @@ export class HandConverterService {
             // if last index join accumulator to return string
             if (index === arr.length - 1) previousValue = previousValue.join('\n');
             return previousValue
-        }, []) as string
+        }, [])
          
     }
 
-    private createNewMetadata(metaData: string): string {
-        let matches = this.stakesRegExp.exec(metaData)
+    private replaceSeatsAndBlindsData(hand: string): string {
+        const handArr = hand.trim().split('\n');
+
+        const start = 2;
+        const end = handArr.indexOf('*** HOLE CARDS ***');
+
+        return handArr.reduce((previousValue: any, currentValue: string, index, arr) => {
+
+            if (index < end && index > start) {
+                currentValue = this.createNewStakesString(currentValue);
+                currentValue = this.createNewBlindsString(currentValue);
+            }
+
+            if (index === arr.length - 1) previousValue = previousValue.join('\n');
+            return previousValue
+        }, []);
+        
+    }   
+
+    private createNewBlindsString(originalString: string): string {
+        let matches = this.blindsRegExp.exec(originalString);
+        if (!matches) return originalString;
+
+        /*
+         ...blinds will capture 3 capture group (post sb, posts bb or posts sb & bb)
+            out of which only one will return truthy value
+        */
+        let [blindString, ...blinds ] = matches;
+        let oldBlind = blinds.filter(blind => blind);
+        let newBlind = +oldBlind / this.stakeModifier
+
+        return blindString.replace(oldBlind, newBlind)
+    }
+
+    private createNewStakesString(originalString: string): string {
+        let matches = this.stacksRegExp.exec(originalString);
+        if (!matches) return originalString;
+
+        // old values
+        let [stackString, stackSize] = matches;
+
+        // new values
+        let newStackSize = +stackSize / this.stakeModifier;
+        let newStackString = stackString.replace(stackSize, newStackSize);
+        
+        return originalString.replace(stackString, newStackString)
+    }
+
+
+    private createNewMetadata(originalString: string): string {
+        let matches = this.stakesRegExp.exec(originalString)
+        if (!matches) return originalString;
+
         let [stakesString, sb, bb] = matches;
+
+        // will be used in the all pipe-transforming functions to scale the hand
+        this.stakeModifier = +bb;
 
         let newStakesString = stakesString
             .replace(sb, '0.5')
             .replace(bb, '1')
 
-        return metaData.replace(stakesString, newStakesString);
+        return originalString.replace(stakesString, newStakesString);
     }
 
 
