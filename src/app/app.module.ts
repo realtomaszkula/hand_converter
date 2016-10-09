470ce4f006bb20bec574e8f1b2bd5c18f1c8033c
Tomasz Kula <tomaszkula07@gmail.com>
2016-10-10 01:20:26 +0200

    Deployed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,25 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from "@angular/router"
 
+import { AngularFireModule } from 'angularfire2';
+
 import { AppComponent } from './app.component';
 import { FileReaderModule } from './file-reader/file-reader.module';
 import { HandConverterRoutingModule } from './app-routing.module';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyCxYiW9ox8UEg9Meoyvih4Unv-iXtvFqZA",
+    authDomain: "hand-converter.firebaseapp.com",
+    databaseURL: "https://hand-converter.firebaseio.com",
+    storageBucket: "hand-converter.appspot.com",
+    messagingSenderId: "434354935041"
+};
 
 @NgModule({
   declarations: [
     AppComponent  ],
   imports: [
+    AngularFireModule.initializeApp(firebaseConfig),
     HandConverterRoutingModule,
     BrowserModule,
     FormsModule,
@@ -22,3 +33,4 @@ import { HandConverterRoutingModule } from './app-routing.module';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,5 +1,6 @@
 // Typings reference file, see links for more information
 // https://github.com/typings/typings
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
-
+declare var require: any;
+declare var module: any;
 declare var System: any;
